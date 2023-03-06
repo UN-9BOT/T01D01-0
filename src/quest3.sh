da79cf1d6bcfb40e355f48a43c1798b44f13f3f0
Yajaira Hercules <un.9bot@gmail.com>
2023-03-06 14:47:18 +0300

    3

---
+++ src/quest3.sh
@@ -0,0 +1,9 @@
+mv door_management_fi door_management_files
+cd door_management_files
+mkdir door_configuration
+mv *.conf door_configuration
+mkdir door_logs
+mv *.log door_logs
+mkdir door_map
+mv *.1 door_map
+chmod 777 ai_door_control.sh
